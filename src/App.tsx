a9587867c135dcca701d13cc421ff55efa44a2eb
hirwaNkevin <hirwankevin@gmail.com>
2023-10-16 11:25:23 +0200

    add basic layout to wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import "./css/App.css";
 function App() {
   return (
     <>
-      <div className="container">
+      <div id="wrapper" className="container">
         <header>
           <SideBar />
         </header>
