1a222c83d1a6c40f7e094ba9469f27e4716ee771
hirwaNkevin <hirwankevin@gmail.com>
2023-10-16 11:21:23 +0200

    set overall structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import SideBar from "./components/SideBar";
 import "./css/App.css";
 
 function App() {
   return (
     <>
-      <div className="container"></div>
+      <div className="container">
+        <header>
+          <SideBar />
+        </header>
+        <main>
+          <h1>Main</h1>
+        </main>
+        <aside id="trends">
+          <h2>aside</h2>
+        </aside>
+        <div id="messaging">
+          <h2>Messaging</h2>
+        </div>
+      </div>
     </>
   );
 }
