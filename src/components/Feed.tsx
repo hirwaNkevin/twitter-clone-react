51034515dcdffd4ea4105d65afed4abdf068ef83
hirwaNkevin <hirwankevin@gmail.com>
2023-10-20 14:32:22 +0200

    add basic content for layout

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,5 +1,23 @@
 const Feed = () => {
-  return <></>;
+  return (
+    <>
+      <FeedMenu />
+    </>
+  );
+};
+
+const FeedMenu = () => {
+  return (
+    <div id="feed-menu">
+      <a href="/">
+        <h2>Home</h2>
+      </a>
+      <nav>
+        <a href="#">Lorem</a>
+        <a href="#">Ipsum</a>
+      </nav>
+    </div>
+  );
 };
 
 export default Feed;
