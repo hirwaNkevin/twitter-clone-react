fcbb30b97f0c6e11db286392beb50b3c7c34bb23
hirwaNkevin <hirwankevin@gmail.com>
2023-10-23 19:06:08 +0200

    add input field and submit button with accessories div

--- src/components/QuickPostComponent.tsx
+++ src/components/QuickPostComponent.tsx
@@ -1,7 +1,35 @@
+// import {
+//   faCheckCircle,
+//   faRepeat,
+//   faChartBar,
+//   faUpload,
+// } from "@fortawesome/free-solid-svg-icons";
+
+// import { faHeart, faCommentAlt } from "@fortawesome/free-regular-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const QuickPostComponent = () => {
   return (
     <div>
-      <h2>HEllo quick post</h2>
+      <div className="post">
+        <div className="img-wrapper-account">
+          <img
+            src="/src/assets/Kevin Hirwa Nzitatira.jpeg"
+            alt="your profile picture"
+          />
+        </div>
+        <div className="post-content">
+          <form action="#">
+            <input type="text" name="post-content" id="post-input" />
+            <div className="flex">
+              <div className="accesories"></div>
+              <button type="submit" className="post-submit-btn">
+                Post
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
