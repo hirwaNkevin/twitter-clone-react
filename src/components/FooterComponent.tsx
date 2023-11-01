cf2484f1d19a06297fb22c98105d13ea7ab37353
hirwaNkevin <hirwankevin@gmail.com>
2023-11-01 12:22:02 +0200

    add html code to component file

--- src/components/FooterComponent.tsx
+++ src/components/FooterComponent.tsx
@@ -1,5 +1,33 @@
+import { faEllipsis } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const FooterComponent = () => {
-  return <div>FooterComponent</div>;
+  return (
+    <footer>
+      <ul className="footer-links">
+        <li>
+          <a href="#">Terms of Service</a>
+        </li>
+        <li>
+          <a href="#">Privacy Policy</a>
+        </li>
+        <li>
+          <a href="#">Cookie Policy</a>
+        </li>
+        <li>
+          <a href="#">Accessibility</a>
+        </li>
+        <li>
+          <a href="#">Ads info</a>
+        </li>
+        <li>
+          <a href="#">More</a>
+          <FontAwesomeIcon icon={faEllipsis} />
+        </li>
+        <li>&copy; 2023 X Corp.</li>
+      </ul>
+    </footer>
+  );
 };
 
 export default FooterComponent;
