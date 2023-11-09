213fa7d25d52ea4069bd4e3d195fe7a423615028
hirwaNkevin <hirwankevin@gmail.com>
2023-11-09 15:46:06 +0200

    add nav to messages

--- src/components/MessagingComponent.tsx
+++ src/components/MessagingComponent.tsx
@@ -1,5 +1,27 @@
+import { faArrowUp, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import {} from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const MessagingComponent = () => {
-  return <div>MessagingComponent</div>;
+  return (
+    <>
+      <nav id="messages-nav">
+        <h2>Messages</h2>
+        <ul>
+          <li>
+            <a href="#">
+              <FontAwesomeIcon icon={faEnvelope} />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <FontAwesomeIcon icon={faArrowUp} />
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
 };
 
 export default MessagingComponent;
