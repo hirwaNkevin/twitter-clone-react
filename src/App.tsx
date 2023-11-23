ec14b13fe8576d47e4dc0df12842cc23453f8fb6
hirwaNkevin <hirwankevin@gmail.com>
2023-11-23 17:40:13 +0200

    reset project

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,9 @@
-import Feed from "./components/Feed";
-import SideBar from "./components/SideBar";
-import Trends from "./components/SideBarTrendsComponent";
-import Footer from "./components/FooterComponent";
-import Messaging from "./components/MessagingComponent";
-import "./css/App.css";
-
 function App() {
   return (
     <>
       <div id="wrapper" className="container">
-        <header>
-          <SideBar />
-        </header>
-        <main>
-          <Feed />
-        </main>
-        <aside id="trends">
-          <Trends />
-          <Footer />
-        </aside>
-        <div id="messaging">
-          <Messaging />
-        </div>
+        <header>header</header>
+        <main>main</main>
       </div>
     </>
   );
--- src/components/LoadedPostsComponent.tsx
+++
@@ -1,9 +0,0 @@
-const LoadedPosts = () => {
-  return (
-    <a href="#" id="loaded-posts">
-      Show 105 posts
-    </a>
-  );
-};
-
-export default LoadedPosts;
