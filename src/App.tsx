e2fd8661ef6c89bb7291af45554a722cd87a920d
hirwaNkevin <hirwankevin@gmail.com>
2023-12-04 16:30:44 +0200

    restore components

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,30 @@
-// import { faXTwitter } from "@fortawesome/free-brands-svg-icons";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import NavLink from "./components/NavLinkComponent";
-// import { faHome } from "@fortawesome/free-solid-svg-icons/faHome";
-// import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { faXTwitter } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import NavLink from "./components/NavLinkComponent";
+import { faHome } from "@fortawesome/free-solid-svg-icons/faHome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 function App() {
   return (
     <div id="wrapper" className="container sm:container mx-auto">
       <header>
-        {/* <div id="logo">
+        <div id="logo">
           <a href="">
             <FontAwesomeIcon icon={faXTwitter} />
           </a>
-        </div> */}
+        </div>
         <nav>
-          <ul></ul>
+          <ul>
+            <NavLink name="Home" icon={<FontAwesomeIcon icon={faHome} />} />
+            <NavLink
+              name="Search"
+              icon={<FontAwesomeIcon icon={faMagnifyingGlass} />}
+            />
+            <NavLink
+              name="Communities"
+              icon={<FontAwesomeIcon icon={faHome} />}
+            />
+            <NavLink name="Home" icon={<FontAwesomeIcon icon={faHome} />} />
+          </ul>
         </nav>
       </header>
       <main>main</main>
